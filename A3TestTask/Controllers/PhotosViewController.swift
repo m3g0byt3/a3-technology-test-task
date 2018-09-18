2e9b88cedb9925034b628f86c69caf39728e3435
m3g0byt3 <m3g0byt3@gmail.com>
2018-09-18 15:21:19 +0300

    Inject `PhotosService` in `PhotosViewController`

--- A3TestTask/Controllers/PhotosViewController.swift
+++ A3TestTask/Controllers/PhotosViewController.swift
@@ -15,4 +15,32 @@ final class PhotosViewController: UIViewController {
 
     // swiftlint:disable:next implicitly_unwrapped_optional
     var user: User!
+
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    var photosService: PhotosServiceProtocol!
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchData()
+    }
+
+    // MARK: - Private API
+
+    private func fetchData() {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        // TODO: change page number in request
+        photosService.getPhotos(user: user, page: 0) { [weak self] result in
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            switch result {
+            case .success(let photos):
+                print(photos)
+            case .failure(let error):
+                let title = Constants.Strings.errorTitle
+                let message = Constants.Strings.errorMessage + error.localizedDescription
+                self?.presentAlert(title: title, message: message)
+            }
+        }
+    }
 }
