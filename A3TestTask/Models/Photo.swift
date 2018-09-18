f01aa956467506165726da8c9f3d0788446c2be3
m3g0byt3 <m3g0byt3@gmail.com>
2018-09-18 15:17:43 +0300

    Add `Photo` model

---
+++ A3TestTask/Models/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  A3TestTask
+//
+//  Created by m3g0byt3 on 18/09/2018.
+//  Copyright © 2018 m3g0byt3. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Codable {
+
+    let url: URL
+    let thumbnailUrl: URL
+}
