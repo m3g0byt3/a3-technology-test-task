0ed0ff4cb1ef5d2b26be9804c257acfa9c22a08b
m3g0byt3 <m3g0byt3@gmail.com>
2018-09-18 09:34:11 +0300

    Add initial DI assembly

---
+++ A3TestTask/Dependency Injection/ApplicationAssembly.swift
@@ -0,0 +1,57 @@
+//
+//  ApplicationAssembly.swift
+//  A3TestTask
+//
+//  Created by m3g0byt3 on 18/09/2018.
+//  Copyright © 2018 m3g0byt3. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Container with dependencies, provides data to the service locator.
+enum ApplicationAssembly {
+
+    case users(configurator: Configurator<ApplicationAssembly>?)
+    case photos(user: User)
+}
+ 
+// MARK: - AssemblyProtocol protocol conformance
+
+extension ApplicationAssembly: AssemblyProtocol {
+
+    var scene: Presentable {
+        switch self {
+
+        case .users(let configurator):
+            let viewController = UsersViewController.fromNib()
+
+            guard case .users(let userService) = service else {
+                fatalError("Unable to resolve dependency ")
+            }
+
+            viewController.userService = userService
+            viewController.configurator = configurator
+            viewController.navigationItem.title = Constants.Strings.usersTitle
+
+            return UINavigationController(rootViewController: viewController)
+
+        case .photos(let user):
+            return PhotosViewController.fromNib()
+        }
+    }
+
+    var service: Service {
+        switch self {
+
+        case .users:
+            let userProvider = Provider<JSONPlaceholderAPI>(configuration: .shortTimeouts)
+            let userService = JSONPlaceholderUserService(provider: userProvider)
+
+            return Service.users(userService)
+
+        case .photos:
+            fatalError()
+        }
+    }
+}
