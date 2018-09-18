a3d636fa72e70d532e5d6f9085fb90e997faa738
m3g0byt3 <m3g0byt3@gmail.com>
2018-09-18 07:00:53 +0300

    TableView initial setup in `UsersViewController`

--- A3TestTask/Controllers/UsersViewController.swift
+++ A3TestTask/Controllers/UsersViewController.swift
@@ -9,4 +9,36 @@
 import Foundation
 import UIKit
 
-final class UsersViewController: UIViewController {}
+final class UsersViewController: UIViewController {
+
+    // MARK: - IBOutlets and UI
+
+    @IBOutlet private weak var tableView: UITableView!
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    // MARK: - Private API
+
+    private func setupUI() {
+        tableView.register(UITableViewCell.self)
+        tableView.tableFooterView = UIView()
+    }
+}
+
+// MARK: - UITableViewDataSource protocol conformance
+
+extension UsersViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(for: indexPath)
+    }
+}
